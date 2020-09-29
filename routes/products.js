d6a6482c1ad9ed886674af301afb175d0c707cf9
Juan Carlos <jcarlosverase@gmail.com>
2020-09-29 00:22:25 -0500

    Add api key validation

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
 let Product = require('../models/product.model');
+let API_KEY = "4c6275f3-3c21-49fa-8928-ba8c5c935b3a";
 
 router.route('/fetch').post((req, res) => {
+    if (!req.get('API_KEY')) {
+        res.status(400).json({
+            status: 400,
+            message: "Missing Api Key"
+        });
+    } else if (req.get('API_KEY') != API_KEY) {
+        res.status(400).json({
+            status: 400,
+            message: "Invalid Api Key"
+        })
+    }
+
     Product.find()
         .then(products => res.json({
             status: 200,
@@ -18,6 +31,18 @@ router.route('/fetch').post((req, res) => {
 });
 
 router.route('/find').post((req, res) => {
+    if (!req.get('API_KEY')) {
+        res.status(400).json({
+            status: 400,
+            message: "Missing Api Key"
+        });
+    } else if (req.get('API_KEY') != API_KEY) {
+        res.status(400).json({
+            status: 400,
+            message: "Invalid Api Key"
+        })
+    }
+
     const regex = new RegExp(req.body.title, 'i')
     Product.find({
         title: regex
@@ -37,6 +62,18 @@ router.route('/find').post((req, res) => {
 });
 
 router.route('/:id').get((req, res) => {
+    if (!req.get('API_KEY')) {
+        res.status(400).json({
+            status: 400,
+            message: "Missing Api Key"
+        });
+    } else if (req.get('API_KEY') != API_KEY) {
+        res.status(400).json({
+            status: 400,
+            message: "Invalid Api Key"
+        })
+    }
+    
     Product.findById(req.params.id)
         .then(product => res.json({
             status: 200,
